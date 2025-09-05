a15092f75f0ef497cf86ab9908f03ce337233852
Harsh47200 <sharmaharsh472000@gmail.com>
2025-09-05 16:15:34 +0530

    url changes main file

--- src/main/java/org/bisag/jkcip/config/CorsConfig.java
+++ src/main/java/org/bisag/jkcip/config/CorsConfig.java
@@ -28,7 +28,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://mamtaengineering.vercel.app/") // allow frontend
+                        .allowedOrigins("https://mamtaengineering.vercel.app") // allow frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
